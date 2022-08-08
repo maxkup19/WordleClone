d53edbc7a1195ace447c99cad953ad687237f965
Maksym Kupchenko <maxkup19@gmail.com>
2022-08-08 20:41:50 +0200

    Initial Commit

---
+++ WordleClone/WordleCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleCloneApp.swift
+//  WordleClone
+//
+//  Created by Max Kup on 08.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct WordleCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
