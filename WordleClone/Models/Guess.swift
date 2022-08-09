05ea00b1622ae1423585aa15a6d7a32856188d5a
Maksym Kupchenko <maxkup19@gmail.com>
2022-08-09 17:55:59 +0200

    Letter cooler add

--- WordleClone/Models/Guess.swift
+++ WordleClone/Models/Guess.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct Guess {
     let index: Int
     var word = "     "
-    var bgColors = [Color](repeating: .systemBackground, count: 5)
-    var cardFlipped = [Bool](repeating: false, count: 6)
-    var guessLetter: [String] {
+    var bgColors = [Color](repeating: .wrong, count: 5)
+    var cardFlipped = [Bool](repeating: false, count: 5)
+    var guessLetters: [String] {
         word.map { String($0) }
     }
-    
 }
--- WordleClone/Views/GuessView.swift
+++ WordleClone/Views/GuessView.swift
@@ -12,7 +12,7 @@ struct GuessView: View {
     var body: some View {
         HStack(spacing: 3) {
             ForEach(0...4, id: \.self) { index in
-                Text(guess.guessLetter[index])
+                Text(guess.guessLetters[index])
                     .foregroundColor(.primary)
                     .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
                     .background(Color.systemBackground)
