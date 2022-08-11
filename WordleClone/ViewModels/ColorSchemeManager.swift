688506d92667510bdf96ab5c428e74eaabe7e6c3
Maksym Kupchenko <maxkup19@gmail.com>
2022-08-11 21:45:46 +0200

    Refactor

---
+++ WordleClone/ViewModels/ColorSchemeManager.swift
@@ -0,0 +1,24 @@
+//
+//  ColorSchemeManager.swift
+//  WordleClone
+//
+//  Created by Max Kup on 11.08.2022.
+//
+
+import SwiftUI
+
+enum ColorScheme: Int {
+    case unspecified, light, dark
+}
+
+class ColorSchemeManager: ObservableObject {
+    @AppStorage("colorScheme") var colorScheme: ColorScheme = .unspecified {
+        didSet {
+            applyColorScheme()
+        }
+    }
+    
+    func applyColorScheme() {
+        UIWindow.key?.overrideUserInterfaceStyle = UIUserInterfaceStyle(rawValue: colorScheme.rawValue)!
+    }
+}
